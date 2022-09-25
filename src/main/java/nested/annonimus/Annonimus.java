2cc17f0255a050012aa925735e500b2d89fa3691
Serhii Asanov <serge.asanov@gmail.com>
2022-09-25 12:00:54 +0300

    added symbol

--- src/main/java/nested/annonimus/Annonimus.java
+++ src/main/java/nested/annonimus/Annonimus.java
@@ -31,6 +31,6 @@ public class Annonimus {
             public void greetSomeone(String someone) {
                 log.info("Hello, friend {}", someone);
             }
-        }
+        };
     }
 }
